3ae1a5d2448c61ef6dca468bb4fd1352ff384928
szoszo1980 <szabolcs.szlobodnyik@gmail.com>
2022-05-30 15:18:13 +0200

    Change message

--- src/main/java/intro/TrainerMain.java
+++ src/main/java/intro/TrainerMain.java
@@ -2,10 +2,16 @@ package intro;
 
 public class TrainerMain {
     public static void main(String[] args) {
+
+        String reset = "\u001B[0m";
+        String red = "\u001B[31m";
+        String green = "\u001B[32m";
+
         Trainer john = new Trainer();
         john.name = "John Doe";
         john.yearOfBirth = 1980;
 
+        System.out.println(red);
         System.out.println(john.name);
         System.out.println(john.yearOfBirth);
 
@@ -13,11 +19,12 @@ public class TrainerMain {
         jack.name ="Jack Doe";
         jack.yearOfBirth = 1990;
 
+        System.out.println(green);
         System.out.printf("%s year of birth: %d",jack.name,jack.yearOfBirth);
 
         Trainer jack2 =new Trainer();
 
         System.out.printf("\n\n%s year of birth: %d",jack2.name,jack2.yearOfBirth);
-
+        System.out.println(reset);
     }
 }
