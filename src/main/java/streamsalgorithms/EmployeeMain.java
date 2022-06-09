81acb58c7e1dd8e1fbc5055f600ac9a72e12c258
Training <szabolcs.szlobodnyik@gmail.com>
2022-06-09 09:04:32 +0200

    ok

--- src/main/java/streamsalgorithms/EmployeeMain.java
+++ src/main/java/streamsalgorithms/EmployeeMain.java
@@ -12,32 +12,40 @@ public class EmployeeMain {
                 new Employee("Szabolcs",1980),
                 new Employee("ffsfsdsd",1990),
                 new Employee("ffsfsdsd",1995),
-                new Employee("ffsfsdsd",1988)
+                new Employee("ffsfsdsdddddd",1988)
         ));
 
-        System.out.print("Alkalmazottak születési éve összegezve: ");
+        System.out.print("Összegezd az alkalmazottak születési éveit!: ");
         System.out.println(employees.stream().mapToInt(e -> e.getYearOfAge()).sum());
 
-        System.out.print("Alkalmazottak életkor szerint összegezve: ");
+        System.out.print("Összegezd az alkalmazottak életkorát!: ");
         System.out.println(employees.stream().mapToInt(e -> 2022 - e.getYearOfAge()).sum());
 
-        System.out.print("Alkalmazottak száma: ");
+        System.out.print("Add vissza streammel, hány alkalmazott van a listában!: ");
         System.out.println(employees.stream().count());
 
-        System.out.print("Alkalmazottak száma 1990 előtt született: ");
+        System.out.print("Add vissza, hány alkalmazott született 1990-nél korábban!: ");
         System.out.println(employees.stream().filter(employee -> employee.getYearOfAge() < 1990).count());
 
-        System.out.print("Legkorábban született alkalmazott éve: ");
-        System.out.println(employees.stream().min(Comparator.comparingInt(employee -> employee.getYearOfAge())).get().getYearOfAge()
-        );
+        System.out.print("Add vissza a legkorábban született alkalmazott születési évét!: ");
+        System.out.println(employees.stream().min(Comparator.comparingInt(employee -> employee.getYearOfAge())).get().getYearOfAge());
 
-        System.out.print("Legkorábban született alkalmazott neve: ");
-        System.out.println(employees.stream().min(Comparator.comparingInt(employee -> employee.getYearOfAge())).get().getName()
-        );
+        System.out.print("Add vissza a legkorábban született alkalmazott nevét! (Itt szükség lesz a findFirst()\n" +
+                "záróműveletre.: ");
+        System.out.println(employees.stream().min(Comparator.comparingInt(employee -> employee.getYearOfAge())).get().getName());
 
-        System.out.println("Alkalmazottak neve: ");
+        System.out.println("Add vissza egy listában az összes alkalmazott nevét!: ");
         employees.forEach(employee -> System.out.println(employee.getName()));
         System.out.println(employees.stream().map(employee -> employee.getName()).collect(Collectors.toList()));
 
+        System.out.print("Add vissza egy listában az összes, 1990-nél korábban született alkalmazottat!: ");
+        System.out.println(employees.stream().filter(employee -> employee.getYearOfAge() < 1990).map(employee -> employee.getName()).collect(Collectors.toList()));
+
+        System.out.print("Add vissza, hogy igaz-e, hogy minden alkalmazott 1980 előtt született-e?: ");
+        System.out.println(employees.stream().allMatch(employee -> employee.getYearOfAge() < 1980));
+
+        System.out.print("Azelőző két feladat kombinációja: Add vissza egy listában az összes, 1990-nél korábban\n" +
+                "született alkalmazott nevét!: ");
+        System.out.println(employees.stream().filter(employee -> employee.getYearOfAge() < 1990).map(employee -> employee.getName()).collect(Collectors.toList()));
     }
 }
